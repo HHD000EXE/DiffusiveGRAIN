1e9b30c02c3fbe89db70f28673aa7d99dc5ca288
Haodi Hu <49475240+HHD000EXE@users.noreply.github.com>
2024-12-20 02:56:08 -0800

    deleted previous output

--- GranularDiffusion.py
+++ GranularDiffusion.py
@@ -211,8 +211,8 @@ model = UNet()
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
 optimizer = optim.Adam(model.parameters(), lr=1e-4)
-epochs = 3000
-show_epoch = list(range(0, epochs, 10)) + [epochs - 1]
+epochs = 20
+show_epoch = list(range(0, epochs, 5)) + [epochs - 1]
 output_dir = "output"
 os.makedirs(output_dir, exist_ok=True)
 
